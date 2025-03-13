a3f0fec28f00eec4c5488fb368fa81a7b4bdb6ca
sinharakesh <rksinha.btech@gmail.com>
2025-03-13 12:54:21 +0530

    initial commit 1

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -44,6 +44,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
+            {/* version done */}
           </Link>
         );
       })}
